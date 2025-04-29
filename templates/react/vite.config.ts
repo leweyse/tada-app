c6c2b3b32b384e4add17f80dc5ba25af218a47bc
Daryl Castro <69996340+leweyse@users.noreply.github.com>
2025-04-29 21:59:16 +0200

    refactor(template): add default alias (#13)

--- templates/react/vite.config.ts
+++ templates/react/vite.config.ts
@@ -1,7 +1,17 @@
+import { fileURLToPath } from 'node:url';
+
 import react from '@vitejs/plugin-react-swc';
+import { dirname, resolve } from 'pathe';
 import { defineConfig } from 'vite';
+
+export const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
 });
