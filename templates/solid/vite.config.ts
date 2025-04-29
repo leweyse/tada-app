0fccbb636c9af328bb065d15af5a6847088d30b3
Daryl Castro <darylcastro67@gmail.com>
2025-04-29 21:56:51 +0200

    refactor(template): set default alias

--- templates/solid/vite.config.ts
+++ templates/solid/vite.config.ts
@@ -1,6 +1,16 @@
+import { fileURLToPath } from 'node:url';
+
+import { dirname, resolve } from 'pathe';
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
+export const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   plugins: [solid()],
 });
